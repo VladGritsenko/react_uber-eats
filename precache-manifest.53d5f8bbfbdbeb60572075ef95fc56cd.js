45816a308d6a1d188608c073b487a0e70ebcbed8
Vlad Grytsenko <vladik.grytsenko2017@gmail.com>
2019-11-11 21:12:11 +0200

    Updates

--- precache-manifest.53d5f8bbfbdbeb60572075ef95fc56cd.js
+++ precache-manifest.53d5f8bbfbdbeb60572075ef95fc56cd.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "85071e27399260ecefa923f7ea177676",
+    "revision": "74cbf2e0ccc4cfe560c74d497f63e071",
     "url": "/react_uber-eats/index.html"
   },
   {
-    "revision": "0b742b5127f0b3a2c6a3",
+    "revision": "e4ba72ba13608c148d06",
     "url": "/react_uber-eats/static/css/main.65938976.chunk.css"
   },
   {
-    "revision": "b5c5beb43fab1ff7f479",
-    "url": "/react_uber-eats/static/js/2.ec295295.chunk.js"
+    "revision": "3a593895592cac0571bd",
+    "url": "/react_uber-eats/static/js/2.8ef6646e.chunk.js"
   },
   {
-    "revision": "0b742b5127f0b3a2c6a3",
-    "url": "/react_uber-eats/static/js/main.c52d50dd.chunk.js"
+    "revision": "e4ba72ba13608c148d06",
+    "url": "/react_uber-eats/static/js/main.233eee7b.chunk.js"
   },
   {
     "revision": "183d5977488a46d51899",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_uber-eats/precache-manifest.4831411afdd49c016f287f8976f8d4d0.js"
+  "/react_uber-eats/precache-manifest.53d5f8bbfbdbeb60572075ef95fc56cd.js"
 );
 
 self.addEventListener('message', (event) => {
